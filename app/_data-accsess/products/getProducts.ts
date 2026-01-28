b2076477f798d74cc3f89209aafe5e4ee949a00d
Luis Gustavo <luisgustavo.nunes@icloud.com>
2026-01-28 12:03:22 -0300

    feat: add products table

---
+++ app/_data-accsess/products/getProducts.ts
@@ -0,0 +1,6 @@
+import { db } from "@/app/_lib/prisma";
+import { products } from "@prisma/client";
+
+export const getProducts = async (): Promise<products[]> => {
+    return db.products.findMany({});
+};
